423aac8dc16c15b57686e3f47609b16f27323752
xionglei <xionglei_51@163.com>
2022-11-21 20:33:29 +0800

    处理

---
+++ src/main/java/com/example/springbootdemotest/SpringbootDemoTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootdemotest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoTestApplication.class, args);
+	}
+
+}
